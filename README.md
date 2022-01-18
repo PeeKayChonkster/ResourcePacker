9521286695bc1d2f724ec85994d6da474f1a1e36
chonk <artstudioexp@gmail.com>
2022-01-18 17:23:54 +0300

    It seems working.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ResourcePacker  
+Simple resource packer using Resman.
+Invokes through terminal/console. First argument is a path to the folder to pack, second - out folder.
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "resman.hpp"
+
+int main(int argc, char* argv[])
+{
+    switch (argc)
+    {
+        case 1:
+            std::cerr << "Invalid first argument.\n";
+            return -1;
+        case 2:
+            if(!chen::Resman::packFolder(argv[1]))
+            {
+                std::cerr << "Couldn't load folder.\n";
+                return -1;
+            }
+            break;
+        default:
+            if(!chen::Resman::packFolder(argv[1], argv[2])) 
+            {
+                std::cerr << "Couldn't load folder.\n";
+                return -1;
+            }
+            break;
+    }
+
+    return 0;
+}
